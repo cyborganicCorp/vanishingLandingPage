383e49576cbedada053789a0e4aae63c9bdf6cf9
cassidy <cyborganicmusic@gmail.com>
2023-05-21 15:08:06 -0700

    testing the loading of images and video on mobile

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log('hello');
-/*window.onload = () => {
+window.onload = () => {
     setTimeout(() => {
         const loader = document.querySelector('.loader');
         const video = document.querySelector('.video');
@@ -26,7 +26,7 @@ console.log('hello');
         enterButton.classList.remove('hidden');
         window.addEventListener('click', enterSite);
     }, 2000)
-}*/
+}
 
 function enterSite(){
     console.log('the enter button has been clicked');
